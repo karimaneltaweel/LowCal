dbd611e536d8d80c009dc382c08bf49f0bf68c80
kariman eltaweel <kariman.hesham0@gmail.com>
2023-06-04 11:42:35 +0300

    section two

---
+++ LowCal/Controller/Cell/SectionTwo+delegate.swift
@@ -0,0 +1,37 @@
+//
+//  SectionTwo+delegate.swift
+//  LowCal
+//
+//  Created by kariman eltawel on 04/06/2023.
+//
+
+import UIKit
+extension SectionTwoCell:UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "month", for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: monthCollection.layer.frame.width , height:  monthCollection.layer.frame.height * 0.5)
+    }
+    
+    
+}
+
+extension SectionTwoCell:UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "maelchoice", for: indexPath)
+        return cell
+    }
+    
+    
+}
--- LowCal/Controller/Cell/SectionTwoCell.swift
+++ LowCal/Controller/Cell/SectionTwoCell.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class SectionTwoCell: UITableViewCell {
 
+    @IBOutlet weak var monthCollection: UICollectionView!{
+        didSet{
+            monthCollection.dataSource = self
+            monthCollection.delegate = self
+        }
+    }
+    @IBOutlet weak var mealsChoice: UITableView!{
+        didSet{
+            mealsChoice.dataSource = self
+            mealsChoice.delegate = self
+        }
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
